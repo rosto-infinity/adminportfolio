267974545cf4f77b9f915e3fea8186fe42375ff2
ROSTAND LELE <rostoinfocus@gmail.com>
2025-06-18 16:45:51 +0100

    gestion middleware

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,17 @@ use App\Http\Controllers\Admin\DashboardController;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home-page');
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin-dashboard');
+Route::middleware(['auth',"admin"])->group(function (){
 
-Route::get('/admin/abouts', [AboutController::class, 'edit'])->name('edit-about');
-Route::patch('/admin/abouts', [AboutController::class,'update'])->name('update-about');
-
-Route::get('/admin/medias', [MediaController::class,'index'])->name('index-media');
-Route::post('/admin/medias', [MediaController::class,'store'])->name("store-medias");
-Route::delete('/admin/medias/{id}', [MediaController::class,'destroy'])->name("destroy-medias");
+    Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin-dashboard');
+    
+    Route::get('/admin/abouts', [AboutController::class, 'edit'])->name('edit-about');
+    Route::patch('/admin/abouts', [AboutController::class,'update'])->name('update-about');
+    
+    Route::get('/admin/medias', [MediaController::class,'index'])->name('index-media');
+    Route::post('/admin/medias', [MediaController::class,'store'])->name("store-medias");
+    Route::delete('/admin/medias/{id}', [MediaController::class,'destroy'])->name("destroy-medias");
+});
 
 
 
